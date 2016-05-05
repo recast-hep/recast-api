400812ed34b2c13a211b953ce9730f320868f11b
Lukas Heinrich <lukas.heinrich@gmail.com>
2016-05-05 14:41:36 -0400

    fix deps

--- setup.py
+++ setup.py
@@ -1,17 +1,18 @@
 from setuptools import setup, find_packages
 
 setup(
-  name = 'recast-api',
-  version = '0.0.1',
-  description = 'RECAST API',
-  url = 'http://github.com/recast-hep/recast-api',
-  author = 'Kyle Cranmer, Lukas Heinrich',
-  author_email = 'cranmer@cern.ch, lukas.heinrich@cern.ch',
-  packages = find_packages(),
-  install_requires = 
-  ['requests',
-   'werkzeug',
-   'boto3',
-   'termcolor',
+    name = 'recast-api',
+    version = '0.0.1',
+    description = 'RECAST API',
+    url = 'http://github.com/recast-hep/recast-api',
+    author = 'Kyle Cranmer, Lukas Heinrich',
+    author_email = 'cranmer@cern.ch, lukas.heinrich@cern.ch',
+    packages = find_packages(),
+    install_requires = [
+        'requests',
+        'werkzeug',
+        'boto3',
+        'termcolor',
+        'pyyaml'
    ]
 )
