108d8ce50012a6b1db1e25cd5143c61d8bfc6fdf
Christian <borachristian@gmail.com>
2016-07-19 09:53:39 -0500

    analysis custom query func

--- recastapi/analysis/get.py
+++ recastapi/analysis/get.py
@@ -10,5 +10,18 @@ def analysis(uuid = None):
     
     single_analysis = '/{}'.format(uuid) if uuid else ''
     url = '{}{}'.format(recastapi.ENDPOINTS['ANALYSIS'], single_analysis)
-    return recastapi.get(url)
-
+    responses = recastapi.get(url)
+    if responses.has_key('_items'):
+        return responses['_items']
+    else:
+        return responses
+    
+def query(query=None):
+    """ customized query. """
+    url = '{}{}'.format(recastapi.ENDPOINTS['REQUESTS'], query)
+    response = recastapi.get(url)
+    if response.has_key('_items'):
+        return response['_items']
+    else:
+        return response
+    
