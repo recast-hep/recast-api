998422bb6d25a2c61bccd87da6a4cb310cfcfb55
Christian <borachristian@gmail.com>
2016-07-22 16:39:47 -0500

    new request class

--- samples/api.py
+++ samples/api.py
@@ -1,120 +1,93 @@
 import json
 import logging
 import os
-import Queue
 import recastapi.request.get
 import recastapi.request.post
 import recastapi.response.post
 import recastapi.response.get
 import yaml
 
-class RecastRequest(object):
-    """ A helper client stateful class for the Request API endpoint.
-    
-    Container for a single request, and keeps history of actions performed and state.
-    """
+
+class RecastApi(object):
+    """ Helper class to POST requests and responses. """
     def __init__(self, request_id=None, json_file=None):
-        self.input = None
         self.all_responses = []
         self.request_id = request_id
-        self.staged_point_request = Queue.Queue()
-        self.staged_basic_request = Queue.Queue()
-
+        self.staged_point_request = {}
+        self.staged_basic_request = {}
         logging.basicConfig(level=logging.DEBUG,
                             format='[%(levelname)s] %(message)s')
         logging.info('Initialization')
-        self.print_sizes()
         if json_file:
-            pass
-    def print_sizes(self):
-        logging.info('point request size: {}'.format(self.staged_point_request.qsize()))
-        logging.info('basic request size: {}'.format(self.staged_basic_request.qsize()))
+            self.read_file(json_file)
+
+    def printout(self):
+        """ Printout the staged parameters. """
+        logging.info('point request: {}'.format(self.staged_point_request))
+        logging.info('basic request: {}'.format(self.staged_basic_request))
         
-    def current_response(self):
+    def current_responses(self):
+        """ Prints latest response from API. """
         if self.all_responses:
             return self.all_responses[-1]
         logging.warning('Responses empty!')
-        #raise Exception('No response yet!')
 
     def read_file(self, filename):
-        """ Reads dumped file and fills queues
-
-        example file format:
-        {'request_id':
-        'staged_response':
-        'staged_basic_response':
-        }
-        """
+        """ Reads file and populates objects. """
         if not os.path.isfile(filename):
             raise Exception('Filename does not exist!')
 
-        f = open(filename)
-        data = yaml.load(f)
-        f.close()
+        with open(filename) as json_file:
+            data = json.load(json_file)
         try:
             self.request_id = data['request_id']
-            response = data['staged_response']
-            response_l = response.split(',')
-            for i in response_l:
-                self.staged_point_request.put(int(i))
-            basic = data['staged_basic_response']
-            basic_l = basic.split(',')
-            for i in basic_l:
-                self.staged_basic_request.put(int(i))
+            self.staged_point_request = data['point_request']
+            logging.info('Read data: {}'.format(data))
         except Exception, e:
             print e
-            raise Exception(e)
-    
+            raise Exception('File not correctly formatted!')
+            
     def dump(self, filename=None):
-        """ Writes state of class to file.
-        
-        """
+        """ Dumps objects into files. """
         if not filename:
-            filename = 'file_request_{}.json'.format(self.request_id)
+            filename = 'request_file_{}.json'.format(self.request_id)
         logging.info('filename: {}'.format(filename))
         data = {}
         data['request_id'] = self.request_id
-        staged_response = ''
-        while not self.staged_point_request.empty():
-            el = self.staged_point_request.get()
-            logging.debug('adding {}'.format(el))
-            staged_response += '{},'.format(el)
-        data['staged_response'] = staged_response
-
-        staged_basic_response = ''
-        while not self.staged_basic_request.empty():
-            el = self.staged_basic_request.get()
-            logging.debug('adding {}'.format(el))
-            staged_basic_response += '{},'.format(el)
-        data['staged_basic_response'] = staged_basic_response
-        logging.info('Content dumped: {}'.format(data))
+        data['point_request'] = self.staged_point_request
+        logging.info('Dumping to file: {}'.format(json.dumps(data)))
+        # data dict to file
         with open(filename, 'w') as f:
             json.dump(data, f)
-                
-    def add_parameter_from_file(self, param_file='samples/param_data.yaml'):
-        """ function to add parameters
-
-        saves point_request_id and basic_request_id into a staging area/Queue
-        to be used later to add
+        
+    def add_point_from_file(self, param_file='samples/param_data.yaml'):
+        """ Function to add point requests.
+        
+        added and saved into dict that can later be retrieved to use
+        for adding.
         """
         f = open(param_file)
         param_data = yaml.load(f)
         f.close()
 
+        # loop through file and fill 
         response = []
-
-        for parameter in param_data:
+        staged_param = {}
+        for i, parameter in enumerate(param_data):
             logging.info('Adding parameter for: {}'.format(parameter['coordinates']))
             point_response = recastapi.request.post.parameter(request_id=self.request_id)
-
             parameter_id = point_response['id']
             logging.debug('Added point request with ID: {}'.format(parameter_id))
-            self.staged_point_request.put(parameter_id)
+            param_key = 'param_{}'.format(i)
+            staged_param[param_key] = {}
+            staged_param[param_key]['point_id'] = parameter_id
+            staged_param[param_key]['data'] = parameter['coordinates']
             
             response.append(point_response)
             response[-1]['coordinate'] = []
-
-            for coordinate in parameter['coordinates']:
+            
+            for coordinate in parameter['coordinates']:                
+                # add coordinates one by one
                 logging.info('\t Adding coordinates: {}'.format(coordinate))
                 coordinate_response = recastapi.request.post.coordinate(
                     parameter_id=parameter_id,
@@ -122,82 +95,91 @@ class RecastRequest(object):
                     coordinate_value=float(coordinate['value'])
                     )
                 logging.debug('Added coordinate with ID: {}'.format(coordinate_response['id']))
-                response[-1]['coordinate'].append(coordinate_response)
-
+                response[-1]['coordinate'].append(coordinate_response)                
+                
             response[-1]['files'] = []
-            for basic in parameter['basicrequests']:
+            staged_basic = {}
+            for j, basic in enumerate(parameter['basicrequests']):
+                
+                # add basic requests one by one
                 logging.info('\t Uploading file: {}'.format(basic))
                 file_response = recastapi.request.post.upload_file(parameter_id=parameter_id,
                                                                    filename=basic)
                 response[-1]['files'].append(file_response)
-                logging.debug('Added archive with ID: {}'.format(file_response['id']))
-                self.staged_basic_request.put(file_response['id'])
-        logging.debug('Added all files!')
+                logging.debug('BASIC  REQUEST: i = {}'.format(j))
+                basic_key = 'basic_{}'.format(j)
+                staged_param[param_key]['basic'] = {}
+                staged_param[param_key]['basic'][basic_key] = {}
+                staged_param[param_key]['basic'][basic_key] = {
+                    'basic_id': file_response['id'],
+                    'data': basic}
+                    
+                logging.debug('basic key: {}'.format(basic_key))
+                staged_basic[basic_key] = {}
+                staged_basic[basic_key]['basic_id'] = file_response['id']
+                staged_basic[basic_key]['data'] = basic
+
+        self.staged_point_request = staged_param
+        self.staged_basic_request = staged_basic
+        logging.debug('Added all files')
         self.all_responses.append(response)
 
-    def add_point_response(self, response_file, archive):
-        """ Adds point response to staged point_request.
-        """
-        if not self.staged_point_request.empty():
-            point_request_id = self.staged_point_request.get()
-            response = {}
-            logging.info('Creating a scan response')
-            scan_response = recastapi.response.post.response(request_id=self.request_id)
-            logging.info('Adding a point response to point request id: {}'.format(
-                point_request_id))
-            response = recastapi.response.post.parameter_response(
-                yaml_file=response_file,
-                point_request_id=point_request_id,
-                scan_response_id=scan_response['id'],
-                filename=archive)
-            logging.debug('Added point response with ID: {}'.format(response['id']))
-            response['scan_response'] = scan_response
-            self.all_responses.append(response)
-            return True
-        logging.error('No staged point request available!')
-        return False
-
-    def add_basic_response(self, basic_response_file, archive):
-        """ Adds basic response to staged basic_request.
-
-        """
-        print 'Function not currently implemented, returning....'
-        return
-        if not self.staged_basic_request.empty():
-            basic_request_id = self.staged_point_request.get()
-            response = {}
-            # first get the point request id
-            logging.debug('checking if basic response has point request id')
-
-    def create_this_request(self, **kwargs):
-        """ to create a request for this class.
+    def add_point_response(self, key_name, response_file, archive):
+        """ Adds point response to staged point_request. """
         
-        function used if the request class wasn't assigned id 
-        at initialization stage.
-        """
-        try:
-            analysis_id = kwargs['analysis_id']
-            title = kwargs['title']
-            description_model = kwargs['description_model']
-            reason_for_request = kwargs['reason_for_request']
-            additional_information = kwargs['additional_information']
-            status = kwargs['status']
-            file_path = kwargs['file_path']
-            parameter_value = kwargs['parameter_value']
-            parameter_title = kwargs['parameter_title']
-        except Exception, e:
-            print e
-            print "Data not added"
-            return False
-
-        response = recastapi.request.post.request(analysis=analysis,
-                                                  title=title,
-                                                  description_model=description_model,
-                                                  reason_for_request=reason_for_request,
-                                                  additional_information=additional_information,
-                                                  status=status,
-                                                  file_path=file_path,
-                                                  parameter_value=parameter_value,
-                                                  parameter_title=parmeter_title)
+        # find point_request_id given key name
+        point_request_id = 0
+        if self.staged_point_request.has_key(key_name):
+            point_request_id = self.staged_point_request[key_name]['point_id']
+        else:            
+            logging.error('Key not found!')
+            raise Exception('Key name not found!')        
+        response = {}
+        logging.info('Creating a scan response')
+        scan_response = recastapi.response.post.response(request_id=self.request_id)
+        logging.info('Adding a point response to point id: {}'.format(point_request_id))
+        
+        response = recastapi.response.post.parameter_response(
+            yaml_file=response_file,
+            point_request_id=point_request_id,
+            scan_response_id=scan_response['id'],
+            filename=archive
+            )
+        logging.debug('Added point response with ID: {}'.format(response['id']))
+        response['scan_response'] = scan_response
         self.all_responses.append(response)
         return True
+
+    def add_basic_response(self, point_key_name, key_name, basic_response_data, archive):
+        """Adds basic response to staged basic_request. """
+
+        point_request_id = 0
+        if self.staged_point_request.has_key(point_key_name):
+            point_request_id = self.staged_point_request[point_key_name]['point_id']
+        else:
+            logging.error('Point request key name not found!')
+            raise Exception('Point key name not found!')
+        basic_request_id = 0
+        if self.staged_point_request.has_key(key_name):
+            basic_request_id = self.staged_point_request[point_key_name]\
+                               ['basic'][key_name]['basic_id']
+        else:
+            logging.error('Basic request key name not found!')
+            raise Exception('Basic key name not found!')
+
+        # get point response id given the point request id
+        try:
+            point_response = recastapi.response.get.point_response_by_id(point_request_id)
+            point_response_id = point_response['id']
+        except Exception, e:
+            print e
+            raise Exception('No point response available for this basic request!')
+
+        logging.info('Adding basic request!')
+        responses = recastapi.response.post.basic_response(
+            yaml_file=basic_response_data,
+            point_response_id=point_response_id,
+            basic_request_id=basic_request_id,
+            filename=archive)
+        self.all_responses.append(responses)
+        logging.info('Added basic request')
